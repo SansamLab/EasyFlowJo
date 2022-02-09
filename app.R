30c4b3f84a2eb98d47a7e12a3fa90022a5153ade
sansamcl <63733772+sansamcl@users.noreply.github.com>
2022-02-08 22:41:54 -0600

    added instructions to main page and column selection

--- app.R
+++ app.R
@@ -11,51 +11,75 @@ library(shiny)
 library(shinymeta)
 
 ############
-ui <-  fluidPage(
+ui <- fluidPage(
   tags$button(
-    id = 'close',
+    id = "close",
     type = "button",
     class = "btn action-button",
-    onclick = "setTimeout(function(){window.close();},500);",  # close browser
+    onclick = "setTimeout(function(){window.close();},500);", # close browser
     "Close window"
   ),
   tabsetPanel(
-   tabPanel("Background Subtract",
-     h1("EasyFlowJo"),
-  fileInput("ColDatafile", label = h3("ColData File input")),
-  fileInput("csvFiles", label = h3(".csv File inputs"),multiple=TRUE),
-  verbatimTextOutput("parentGate"),
-  verbatimTextOutput("gates"),
-  uiOutput("download"),
-  uiOutput("downloadCode"),
-  tableOutput("testTable")),
-  tabPanel("Instructions",
-    h1("EasyFlowJo Usage Instructions"),
-    h2("Motivation"),
-    p("The purpose of this shiny app is to easily calculate background subtracted values from bivariate cell cycle flow cytometry data. Once the conversions are made, you can save the data in the form of a .rds file, which can be loaded into R for statistical analysis and plotting."),
-    h2("Test Data"),
-    p("You may access test data from github."),
-    p("https://github.com/SansamLab/EasyFlowJo"),
-    h2("How To Use"),
-    h3("Step 1:  Gate your data in flowjo."),
-    img(src = "image001.jpg"),
-    h3("Step 2:  Export channel values from flowjo to .csv files"),
-    h4("2a.  Select the all parent, G1, and  S populations"),
-    p("Select the parent gate (Single Cells), G1, and S.  Click select equivalent nodes to select those gates for all samples."),
-    img(src = "image003.png"),
-    h4("2b.  Export the channel values"),
-    p("After parent, G1, and S gates are selected for all samples, click Export/Concatenate Populations."),
-    img(src = "image005.png"),
-    img(src = "image007.png"),
-    h3("Step 3:  Create a colData file describing your data"),
-    img(src = "image009.png"),
-    h3("Step 4:  Load colData into shiny app"),
-    img(src = "image011.png"),
-    h3("Step 5:  Load channel value .csv files into shiny app"),
-    img(src = "image013.png"),
-    h3("Step 6:  Download .rds file"),
-    img(src = "image015.png")),
-  tabPanel("Rendered Code",verbatimTextOutput("code"))
+    tabPanel(
+      "Background Subtract",
+      h1("EasyFlowJo"),
+      sidebarLayout(
+        sidebarPanel(fileInput("ColDatafile", label = h3("ColData File input"))),
+        mainPanel(wellPanel(
+          h4("the colData .csv should have the following columns:"),
+          h5("Control,Treated,yaxischannel,DNAchannel,ParentGate"),
+          img(src = "colDataExample.png")
+        ))
+      ),
+      sidebarLayout(
+        sidebarPanel(
+          fileInput("csvFiles", label = h3(".csv File inputs"), multiple = TRUE)
+        ),
+        mainPanel(
+          wellPanel(
+            h4(".csv filenames must have the following format:"),
+            uiOutput(outputId = "csvFileText"),
+            h4("For example, the \"G1\" gate for \"myFile.fcs\" would be:"),
+            uiOutput(outputId = "csvFileTextExample")
+          )
+        )
+      ),
+      verbatimTextOutput("parentGate"),
+      verbatimTextOutput("gates"),
+      uiOutput("download"),
+      uiOutput("downloadCode"),
+      uiOutput("columnSelection"),
+      tableOutput("testTable")
+    ),
+    tabPanel(
+      "Instructions",
+      h1("EasyFlowJo Usage Instructions"),
+      h2("Motivation"),
+      p("The purpose of this shiny app is to easily calculate background subtracted values from bivariate cell cycle flow cytometry data. Once the conversions are made, you can save the data in the form of a .rds file, which can be loaded into R for statistical analysis and plotting."),
+      h2("Test Data"),
+      p("You may access test data from github."),
+      p("https://github.com/SansamLab/EasyFlowJo"),
+      h2("How To Use"),
+      h3("Step 1:  Gate your data in flowjo."),
+      img(src = "image001.jpg"),
+      h3("Step 2:  Export scale values from flowjo to .csv files"),
+      h4("2a.  Select the all parent, G1, and  S populations"),
+      p("Select the parent gate (Single Cells), G1, and S.  Click select equivalent nodes to select those gates for all samples."),
+      img(src = "image003.png"),
+      h4("2b.  Export the channel values"),
+      p("After parent, G1, and S gates are selected for all samples, click Export/Concatenate Populations."),
+      img(src = "image005.png"),
+      img(src = "image007.png"),
+      h3("Step 3:  Create a colData file describing your data"),
+      img(src = "image009.png"),
+      h3("Step 4:  Load colData into shiny app"),
+      img(src = "image011.png"),
+      h3("Step 5:  Load channel value .csv files into shiny app"),
+      img(src = "image013.png"),
+      h3("Step 6:  Download .rds file"),
+      img(src = "image015.png")
+    ),
+    tabPanel("Rendered Code", verbatimTextOutput("code"))
   )
 )
 ###########
@@ -113,6 +137,8 @@ server <- function(input, output, session) {
       # rename DNA and y axis columns to x and y
       csvList <- lapply(csvList,function(df){
         DNAChannel <- gsub("-", ".", cdata$DNAchanel[1])
+        DNAChannel2 <- gsub("-", ".", cdata$DNAchannel[1])
+        DNAChannel <- paste0(DNAChannel,DNAChannel2)
         YChannel <- gsub("-", ".", cdata$yaxischannel[1])
         names(df) <- gsub(DNAChannel, "x", names(df))
         names(df) <- gsub(YChannel, "y", names(df))
@@ -136,40 +162,6 @@ server <- function(input, output, session) {
         gsub("^(.*[_])","",.)%>%unique()%>%.[-which(.==..(parentGate()))]
     }))
   })
-
-  # addIDColumn <- function(csvList){
-  #   lapply(csvList,function(df){
-  #     df$ID <- do.call(paste, c(df, sep = ""))
-  #     df
-  #   })
-  # }
-  #
-  # addGateColumn <- function(csvList){
-  #   lapply(names(csvList),function(nme){
-  #     df <- csvList[[nme]]
-  #     df$gate <- gsub(".csv","",nme,ignore.case=T)%>%gsub("^(.*[_])","",.)
-  #     df
-  #   })
-  # }
-
-
-  # csvList2 <- metaReactive2({
-  #   req(input$csvFiles)
-  #   metaExpr({
-  #     csvList <- lapply(..(csvList()), function(df) {
-  #       cdata <- ..(coldata())
-  #       # add ID column
-  #       df$ID <- do.call(paste, c(df, sep = ""))
-  #       DNAChannel <- gsub("-", ".", cdata$DNAchanel[1])
-  #       YChannel <- gsub("-", ".", cdata$yaxischannel[1])
-  #       names(df) <- gsub(DNAChannel, "x", names(df))
-  #       names(df) <- gsub(YChannel, "y", names(df))
-  #       df
-  #     })
-  #     csvList
-  #   })
-  # })
-
 
   # merge the data tables. this metaReactive2 makes a list of dataframes with all channel values and that csvFilename, fcsFilename, and gate columns
   mergedDataTables <- metaReactive2({
@@ -233,7 +225,7 @@ server <- function(input, output, session) {
       paste0("dataList", ".rds")
     },
     content = function(file) {
-      saveRDS(backgroundSubtracted(), file)
+      saveRDS(backgroundSubtractedSubset(), file)
     }
   )
 
@@ -243,8 +235,8 @@ server <- function(input, output, session) {
   })
 
   output$testTable <- renderTable({
-    req(backgroundSubtracted())
-    head(backgroundSubtracted()[[1]])
+    req(backgroundSubtractedSubset())
+    head(backgroundSubtractedSubset()[[1]],5)
   })
 
   code <- reactive({
@@ -285,7 +277,8 @@ server <- function(input, output, session) {
 "#################################################################",
 "##                     write .rds file                         ##",
 "#################################################################",
-      quote(saveRDS(backgroundSubtracted,"backGroundSubtracted.rds"))
+      invisible(backgroundSubtractedSubset()),
+      quote(saveRDS(backgroundSubtractedSubset,"backGroundSubtracted.rds"))
     ))
   })
 
@@ -300,7 +293,7 @@ server <- function(input, output, session) {
   output$code <- renderPrint({code()})
 
   output$downloadCode <- renderUI({
-    req(backgroundSubtracted())
+    req(backgroundSubtractedSubset())
     downloadButton("downloadCodeH", label = "download code")
   })
 
@@ -312,11 +305,32 @@ server <- function(input, output, session) {
     content = function(file) {
       buildScriptBundle(
         code(), file, render=FALSE
-      #,
-      #  render_args = list(output_format = "pdf_document")
       )
     }
   )
+
+  output$csvFileText <- renderText({
+    HTML(paste0("<b style='color:red'>","export","</b>", "_fcsFileBaseName_","<b style='color:green'>","gate","</b>",".csv"))
+  })
+  output$csvFileTextExample <- renderText({
+    HTML(paste0("<b style='color:red'>","export","</b>", "_myFile_","<b style='color:green'>","G1","</b>",".csv"))
+  })
+  output$columnSelection <- renderUI({
+    req(backgroundSubtracted())
+    selectizeInput('columnSelect',
+                   label="Select the columns to export",
+                   choices=names(backgroundSubtracted()[[1]]),
+                   selected = c("csvFilename","fcsFilename","gate","x","y","y2"),
+                   multiple = TRUE,
+                   options = NULL)
+  })
+  backgroundSubtractedSubset <- metaReactive2({
+    req(output$columnSelection)
+    metaExpr({
+      lapply(..(backgroundSubtracted()),function(df){df[..(input$columnSelect)]})
+    })
+  })
+
 
 }
 
